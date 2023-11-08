c307b1e63e1cdc1f9e301d7b4d80e80a2c55bcca
Thomas Günzel <me@thomasguenzel.com>
2023-11-08 02:29:16 +0100

    add apple sample package

---
+++ SlothCreator/Sources/SlothCreator/Creation/NameGenerator.swift
@@ -0,0 +1,14 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A utility type that generates names for sloths.
+*/
+
+/// A type that generates names for sloths.
+public protocol NameGenerator {
+    /// Generates a name for a sloth.
+    ///
+    /// - parameter seed: A value that influences randomness.
+    func generateName(seed: Int) -> String
+}
---
+++ SlothCreator/Sources/SlothCreator/Creation/SlothGenerator.swift
@@ -0,0 +1,12 @@
+/*
+See the LICENSE.txt file for this sample’s licensing information.
+
+Abstract:
+A utility type that generates sloths.
+*/
+
+/// A type that generates sloths.
+public protocol SlothGenerator {
+    /// Generates a sloth in the specified habitat.
+    func generateSloth(in habitat: Habitat) throws -> Sloth
+}
